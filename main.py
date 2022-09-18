dc47f12c44319fe7d9402d0cb10a0aca0f66a7e6
Moritz <96340080+mol42069@users.noreply.github.com>
2022-09-18 13:51:02 +0200

    Create main.py
    just the first few lines

---
+++ main.py
@@ -0,0 +1,58 @@
+import pygame
+grey = (30, 30, 30)
+pygame.init()
+x = 1920
+y = 1080
+screen_size = (x, y)
+display = pygame.display.set_mode(screen_size, pygame.FULLSCREEN)
+display.fill(grey)
+button_img = pygame.image.load('grey.png').convert_alpha()
+
+
+class Button:
+
+    def __init__(self, x, y, image, scale_x, scale_y):
+        width = image.get_width()
+        height = image.get_height()
+        self.image = pygame.transform.scale(image, (int(width * scale_x),int(height * scale_y)))
+        self.rect = image.get_rect()
+        self.rect.topleft = (x, y)
+        self.clicked = False
+
+    def draw(self):
+        action = False
+        pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and not self.clicked:
+                action = True
+                self.clicked = True
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            action = False
+            self.clicked = False
+
+        display.blit(self.image, (self.rect.x, self.rect.y))
+
+        return action
+
+
+def main():
+
+    running = True
+    clock = pygame.time.Clock()
+
+    exit_button = Button(1920 - 70 * 0.4, 0, button_img, 0.4, 0.3)
+
+    while running:
+        clock.tick(500)
+        if exit_button.draw():
+            running = False
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        pygame.display.update()
+
+
+main()
